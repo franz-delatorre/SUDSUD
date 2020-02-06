aa84a5fc9b90a74f70d73c89041f45baf854c9c6
N-141 <n-141@N-257.local>
2020-02-06 13:49:21 +0800

    SUD: refactored Narrative

--- src/dialogue/Narrative.java
+++ src/dialogue/Narrative.java
@@ -3,9 +3,7 @@ package dialogue;
 import java.util.ArrayList;
 
 public class Narrative {
-    private static final int NARRATIVE_SIZE = 2;
-
-    private ArrayList<String[]> narrative = new ArrayList<>(NARRATIVE_SIZE);
+    private ArrayList<String[]> narrative = new ArrayList<>(2);
     private boolean isNarrated = false;
 
     public Narrative() {
@@ -18,7 +16,7 @@ public class Narrative {
     }
 
     public String[] getNarrative(int index) {
-        if (index > NARRATIVE_SIZE - 1) {
+        if (index > narrative.size() - 1) {
             return new Narrative().getNarrative(index);
         }
         return narrative.get(index);
